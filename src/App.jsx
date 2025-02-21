6265a997ad959cb5d9d699b81cc14354223d618a
Your Name <your_email@example.com>
2025-02-21 19:10:39 +0530

    Initial commit-Added project setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className='bg-red-200 w-1/2 h-3 text-red-300'>
+      <p className='font-extrabold '>hello</p>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
