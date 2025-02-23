200b621b45d88cd0d5cb481af87595261a0ec395
Your Name <your_email@example.com>
2025-02-23 15:09:14 +0530

    Added api and cofigured movieCard with title and poster from tmdb api

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
-function MovieCard() {
+function MovieCard({ title, poster }) {
   return (
-    <div className='w-[11rem] h-[40vh] bg-center bg-cover flex items-end justify-center border rounded-xl hover:scale-110 duration-300 hover:cursor-pointer m-1' style={{backgroundImage:`url(https://th.bing.com/th/id/OIP.Yk1EgVgC7B49-lPscg3e6wHaKe?rs=1&pid=ImgDetMain)`}}>
-        <h1 className='text-center text-white'>Joker</h1>
+    <div
+      className="w-[11rem] h-[40vh] bg-center bg-cover flex items-end justify-center border rounded-xl hover:scale-110 duration-300 hover:cursor-pointer m-1"
+      style={{
+        backgroundImage: `url(https://image.tmdb.org/t/p/w500/${poster})`,
+      }}
+    >
+      <h1 className="text-center text-white">{title}</h1>
     </div>
-  )
+  );
 }
 
-export default MovieCard
+export default MovieCard;
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,24 +1,39 @@
-import React from 'react'
-import MovieCard from './MovieCard'
+import React, { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
+import axios from "axios";
 
 function Movies() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `
+https://api.themoviedb.org/3/movie/popular?api_key=c9faaad8733e99dd34704cf8744eb72b&language=en-US&page=1
+
+`
+      )
+      .then(function (res) {
+        //console.log(res.data.results);
+         setMovies(res.data.results);
+      });
+  }, []);
+
   return (
     <div>
-      <div className='p-5'>
-        <h1 className=' m-2 border-2 rounded-2xl text-2xl font-black text-center'>Trending Movies</h1>
-        <div className='flex flex-wrap '>
-        <MovieCard/>
-        <MovieCard/>
-        <MovieCard/>
-        <MovieCard/>
-        <MovieCard/>
-        <MovieCard/>
-        <MovieCard/>
-        <MovieCard/>
+      <div className="p-5">
+        <h1 className=" m-2 border-2 rounded-2xl text-2xl font-black text-center">
+          Trending Movies
+        </h1>
+        <div className="flex flex-wrap ">
+          {movies.map((movie) => (
+            <MovieCard title={movie.original_title} poster={movie.poster_path} />
+           ))}
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Movies
+export default Movies;
+//c9faaad8733e99dd34704cf8744eb72b
